d562bd7797389a2a463b9cdc7fc76ad265ba6232
gopinadh3541 <39827201+gopinadh3541@users.noreply.github.com>
2018-10-18 12:37:42 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,9 +9,11 @@ import org.junit.runner.RunWith;
  * Created by Karthik on 10/26/2016.
  */
 
-//@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
         glue = "steps")
 public class TestRunner extends AbstractTestNGCucumberTests{
+        System.out.println("Working");
+        
 
 }
