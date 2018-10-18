e17ae932ec40dbf0831eab4279a08e0bd5b23e13
gopinadh3541 <39827201+gopinadh3541@users.noreply.github.com>
2018-10-18 12:57:20 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Karthik on 10/17/2016.
  */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/steps"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
+        glue = "steps")
 public class Hook extends BaseUtil{
 
 
